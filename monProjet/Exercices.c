f5b1beb1da4e533a76def1d65beb24b2d2ec93a9
liah <jecholiahw@gmail.com>
2021-05-21 13:02:42 +0000

    first commit

---
+++ monProjet/Exercices.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	float S,impot;
+	printf("Entrer votre salaire: ");
+	scanf("%d", &S);
+	if(S>=35000)
+	{
+	impot=S*7/100;
+	}
+	else
+	{
+		impot=125;
+	}
+	printf("Impot a payer=%.2f", impot);
+}
---
+++ monProjet/equation_second_degre.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	float a,b,c,d,x0 = 0,x1=0,x2=0;
+	
+	printf("saisir un premier reel :");
+	
+	scanf("%f" ,&a);
+	
+	printf("saisir un second reel : ");
+	
+	scanf("%f" ,&b);
+	
+	printf("saisir un troisieme reel : ");
+	
+	scanf("%f",&c);
+	d=(b*b)-(4*a*c);
+	
+	if(a==0){
+		printf(" l equation n est pas une equation du second degre");
+	}
+	else{
+			if(d<0){
+		printf("pas de solution");
+	}
+	if(d==0){
+		x0= -b/2*a;
+		printf("la solution est %.2f",x0);
+	}
+	if(d>0){
+		x1= (-b-sqrt(d))/(2*a);
+		x2= (-b+sqrt(d))/(2*a);
+		printf(" les solutions de l equation sont %.2f et %.2f :",x1,x2);
+	}
+	}
+
+}
